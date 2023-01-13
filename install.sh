082714ba4e43541df8dbbd3f32305e471eeae926
uesseu <sheepwing@kyudai.jp>
2023-01-13 17:41:35 +0900

    Install was modified

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 cp ~/.bashrc ~/.bashrc_backup
 cat ~/.bashrc_backup | sed -e '/acep_dbfilter/d' > ~/.bashrc
 echo 'export PATH="$HOME/acep_dbfilter/dbfilter:$PATH"' >> ~/.bashrc
-echo 'git pull acep_dbfilter' >> ~/.bashrc
+echo 'cd acep_dbfilter; git pull; cd ~' >> ~/.bashrc
